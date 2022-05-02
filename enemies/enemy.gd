1e48d1539a3e55571217d449a4a743708f6005d4
Fabricio Manrique <fabemanrique@hotmail.com>
2022-05-02 17:36:13 -0300

    Added groups Player and Enemies and added corresponding nodes to their groups. We now detect area entered and exited for enemies

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,12 +1,30 @@
 extends Path2D
 
-onready var follow_path = $PathFollow2D
-
-var current_offset : float = 0
+const PLAYER_GROUP = "Player"
 
 export var speed = 50
 
+onready var follow_path = $PathFollow2D
+
+var current_offset : float = 0
+var player_target = null
+
+func _process(delta):
+	if player_target != null:
+		pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	current_offset += delta * speed
-	follow_path.offset = current_offset
+	if player_target == null:
+		current_offset += delta * speed
+		follow_path.offset = current_offset
+
+
+func _on_DetectionArea2D_body_entered(body):
+	if body.is_in_group(PLAYER_GROUP):
+		player_target = body
+
+
+func _on_DetectionArea2D_body_exited(body):
+	if body.is_in_group(PLAYER_GROUP):
+		player_target = null
