0c7eb5b823399422e76197f8aca63d9b86daaa66
Fabricio Manrique <fabemanrique@hotmail.com>
2022-05-05 09:32:50 -0300

    Implemented a basic box that we can push

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 const PLAYER_GROUP = "Player"
 const LEVEL_GROUP = "Level"
+const BOXES_GROUP = "Boxes"
 
 export var motion_speed : float = 100
 
@@ -19,5 +20,5 @@ func _on_DetectionArea2D_body_entered(body):
 	if body.is_in_group(PLAYER_GROUP):
 		pass # Replace with function body.
 	
-	if body.is_in_group(PLAYER_GROUP) or body.is_in_group(LEVEL_GROUP):
+	if body.is_in_group(PLAYER_GROUP) or body.is_in_group(LEVEL_GROUP) or body.is_in_group(BOXES_GROUP):
 		queue_free()
--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
 export var motion_speed : float = 160 # Pixels/second
+export var push_force : float = 20
 
 var motion : Vector2 = Vector2.ZERO
+
+const FLOOR_MAX_ANGLE = PI/4
+const MAX_SLIDES = 4
+const BOXES_GROUP = "Boxes"
 
 func _process(_delta):
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -11,4 +16,13 @@ func _process(_delta):
 	motion = motion.normalized() * motion_speed
 	
 func _physics_process(_delta):
-	motion = move_and_slide(motion)
+	motion = move_and_slide(motion, Vector2.UP, false, MAX_SLIDES, FLOOR_MAX_ANGLE, false)
+	process_push_boxes()
+	
+func process_push_boxes():
+	for index in get_slide_count():
+		var current_collision = get_slide_collision(index)
+		var current_collider = current_collision.collider
+		
+		if current_collider.is_in_group(BOXES_GROUP):
+			current_collider.apply_central_impulse(-current_collision.normal * push_force)
